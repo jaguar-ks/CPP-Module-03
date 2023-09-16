7315c930aaeae494b2b95fbfa186efdee8013632
jaguar-ks <fahdamineksouss@gmail.com>
2023-09-16 02:19:41 +0100

    INIT : start working on the first exircice on CPP-Module:03

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faksouss <faksouss@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 01:52:18 by faksouss          #+#    #+#             */
+/*   Updated: 2023/09/16 01:59:29 by faksouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include<string>
+#include<iostream>
+
+class ClapTrap {
+    private:
+        std::string Name;
+        int  Health;
+        int  Energy;
+        int  Attack_dmg;
+    public:
+        
+};
